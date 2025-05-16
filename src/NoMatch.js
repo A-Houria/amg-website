02e8dd8f3e1172ff9c1ec2162f6947cb7309e2ba
abdelrahman-houria <amr.houria70@gmail.com>
2025-05-16 20:00:16 +0200

    added nomatch page

--- src/NoMatch.js
+++ src/NoMatch.js
@@ -1,9 +1,17 @@
+import { Link } from "react-router-dom";
 const NoMatch = () => {
-    return ( 
-        <div className="nomatch">
-            <h1>404 Page Not Found!</h1>
-        </div>
-     );
-}
- 
+  return (
+    <div className="nomatch">
+      <div className="not-found-content">
+        <h1>404</h1>
+        <h2>Page Not Found</h2>
+        <p>Oops! The page you're looking for doesn't exist.</p>
+        <Link to="/" className="home-btn">
+          Go Back Home
+        </Link>
+      </div>
+    </div>
+  );
+};
+
 export default NoMatch;
